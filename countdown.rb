7959f1fcde62f2fab09fa7c61586f1429aae7fba
Kelin Field <kfield111@hotmail.com>
2020-08-01 22:35:54 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,6 +3,16 @@ def countdown(number)
   while number > 0
     puts "#{number} SECOND(S)!"
     number -= 1
+  end
+  return "Happy New Year!"
+end
+
+
+def countdown(number)
+  number = 10
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
     sleep(1)
   end
   return "Happy New Year!"
